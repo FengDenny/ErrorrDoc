8f94fc62c45df6004553e14054b6ed551dd02ef4
FengDenny <fengdennyy@gmail.com>
2022-04-22 14:30:48 -0700

    signup / login authentication system is functional

--- server/models/authModel/authModel.js
+++ server/models/authModel/authModel.js
@@ -1,20 +1,75 @@
 const dbo = require("../mongdbConnect");
-
+const { hashPassword } = require("../../controllers/HelperFunction");
 // convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
+const csprng = require("csprng");
 
 exports.signUpFree = async (req, response) => {
-  let db_connect = dbo.getDB();
-  const { email, password } = req.body;
-  let accountInfoMap = {
+  let { email, password } = req.body;
+  const salt = csprng(160, 36);
+  password = hashPassword(`${salt}${password}`);
+  const db_connect = dbo.getDB();
+  const connection = db_connect.collection("users");
+  const userFound = await connection.findOne({ email });
+  const accountInfoMap = {
     email,
     password,
+    salt,
   };
 
-  await db_connect
-    .collection("users")
-    .insertOne(accountInfoMap, function (err, result) {
-      if (err) throw err;
-      response.json(result);
+  if (userFound) {
+    return response.json({
+      message: `${email} has been taken. Please use another email.`,
     });
+  }
+
+  await connection.insertOne(accountInfoMap, function (err, result) {
+    if (err) throw error;
+    response.json({
+      status: "success",
+      message: `${email} has successfully signed up. Please log in.`,
+    });
+  });
+};
+
+exports.login = async (req, response) => {
+  const { email, password } = req.body;
+  const db_connect = dbo.getDB();
+  const connection = db_connect.collection("users");
+  const userFound = await connection.find({ email });
+
+  userFound.toArray((err, docs) => {
+    if (err) {
+      response.json({ message: err.message });
+    }
+
+    if (docs.length > 0) {
+      let hashedPassword = docs[0].password;
+      const salt = docs[0].salt;
+      if (!password) {
+        response.json({ message: "Please enter a password" });
+      }
+      const passwordCheck = hashPassword(`${salt}${password}`);
+
+      if (hashedPassword === passwordCheck) {
+        response.json({
+          status: "success",
+          message: `Welcome back, ${email}!`,
+          data: {
+            ...docs[0],
+          },
+        });
+      } else {
+        response.json({
+          status: "error",
+          message: "Your email or password was incorrect.",
+        });
+      }
+    } else {
+      response.json({
+        status: "error",
+        message: "There was no email associated with an account. ",
+      });
+    }
+  });
 };
--- server/routes/authRoutes/authRoutes.js
+++ server/routes/authRoutes/authRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const authRoutes = express.Router();
 
-const { signUpFree } = require("../../models/authModel/authModel");
-
-// Sign Up Free Routes
+const { signUpFree, login } = require("../../models/authModel/authModel");
 
 authRoutes.post("/auth/signupfree", signUpFree);
+authRoutes.post("/auth/login", login);
 
 module.exports = authRoutes;
