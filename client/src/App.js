b080925323fa6f92403913cccdffe707bb4fffc9
FengDenny <fengdennyy@gmail.com>
2022-05-03 19:54:37 -0700

    created overlay for our Modal Container using useRef

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import "./View/content/App.css";
+import { useRef } from "react";
+import "./View/content/css/App.css";
 import { GlobalStyles } from "./View/content/styled-components/Global.styled.js";
 import { HashRouter as Router, useRoutes } from "react-router-dom";
 import Home from "./View/pages/Home/Home";
@@ -6,15 +7,21 @@ import Navbar from "./View/Utility/Navbar/Navbar";
 import Footer from "./View/Utility/Footer/Footer";
 function App() {
   let routes = useRoutes([{ path: "/", element: <Home /> }]);
+  const overlay = useRef(null);
+  const setOverlay = () => {
+    overlay.current.classList.add("overlay");
+  };
+
   return (
-    <>
+    <div>
       <GlobalStyles />
-      <Navbar />
+      <Navbar setOverlay={setOverlay} />
       <div className='main-container'>
         <main>{routes}</main>
+        <div ref={overlay} />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
