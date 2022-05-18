210da8bf2e3c737a643978eda7ff513947502291
FengDenny <fengdennyy@gmail.com>
2022-05-17 17:16:05 -0700

    removed updated email verification email template

--- server/views/emailTemplate/email.js
+++ server/views/emailTemplate/email.js
@@ -20,55 +20,12 @@ exports.emailVerify = (url) => {
       </table>
           `;
 };
-exports.emailReVerify = (url) => {
-  return `
-          <table align="center" cellpadding="0" cellspacing="0"  >
-          <tr>
-              <td style="padding: 20px 0 30px 0;">
-                  <h1 style="margin: 0; color:red; text-align:center">Please use the following link to activate your email</h1>
-              </td>
-          </tr>
-          <tr>
-              <td style="padding: 20px 0 30px 0;">
-                  <a href="${url}" style="margin: 0; color:gray; width:700px; line-height:20px">Activate your email</a>
-              </td>
-          </tr>
-          <tr>
-            <td style="padding: 20px 0 30px 0;">
-              <p style="margin: 0; color:gray; width:700px; line-height:20px"> If the link doesn't work, please copy and paste this url to activate: ${url}</p>
-            </td>
-          </tr>
-      </table>
-          `;
-};
-
 exports.accountActivated = (loginURL) => {
   return `
     <table align="center" cellpadding="0" cellspacing="0"  >
     <tr>
         <td style="padding: 20px 0 30px 0;">
             <h1 style="margin: 0; color:red; text-align:center">Thank you! Your account has been activated! Please signin!</h1>
-        </td>
-    </tr>
-    <tr>
-        <td style="padding: 20px 0 30px 0;">
-            <a href="${loginURL}" style="margin: 0; color:gray; width:700px; line-height:20px">Signin</a>
-        </td>
-    </tr>
-    <tr>
-      <td style="padding: 20px 0 30px 0;">
-        <p style="margin: 0; color:gray; width:700px; line-height:20px"> If the link doesn't work, please copy and paste this url to sign in: ${loginURL}</p>
-      </td>
-    </tr>
-  </table>
-    `;
-};
-exports.emailReActivated = (loginURL) => {
-  return `
-    <table align="center" cellpadding="0" cellspacing="0"  >
-    <tr>
-        <td style="padding: 20px 0 30px 0;">
-            <h1 style="margin: 0; color:red; text-align:center">Thank you! Your email has been activated!</h1>
         </td>
     </tr>
     <tr>
